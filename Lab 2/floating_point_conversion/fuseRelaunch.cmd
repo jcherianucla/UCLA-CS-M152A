dd5d39b8f7c2c08dd485791370674fb9ca2a24cd
jcherianucla <jcherian@ucla.edu>
2016-06-07 09:32:43 -0700

    Removed some of the unnecessary files from Lab 2

--- Lab 2/floating_point_conversion/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/152/Desktop/floating_point_conversion/fp_converter_TB_isim_beh.exe" -prj "C:/Users/152/Desktop/floating_point_conversion/fp_converter_TB_beh.prj" "work.fp_converter_TB" "work.glbl" 
